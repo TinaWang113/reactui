9f2ba7cf554fbe0f9623fafea3ffb47107da8c50
tinawang113 <yunting.wang@edu.sait.ca>
2020-05-05 19:38:36 -0600

    add btn function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from "./Header";
+import TestPage1 from "./TestPage1.js";
+import TestPage2 from "./TestPage2";
+import  { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/TestPage1" component={TestPage1} />
+          <Route exact path="/TestPage2" component={TestPage2} />
+        </Switch>
+     
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/TestPage2.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+
+class TestPage2 extends Component {
+    handleClickedHeader = () =>{
+        this.props.history.push("/");
+    };
+    handleClickedTestPage1 = () =>{
+        this.props.history.push("/TestPage1");
+    };
+
+    render() {
+        return (
+            <div>
+               <h2>Self-Learning react - testing page</h2>
+                <button onClick={this.handleClickedHeader} >Home</button> 
+                <button onClick={this.handleClickedTestPage1} >TestPage1</button> 
+            </div>
+        )
+    }
+}
+
+
+export default withRouter(TestPage2);
